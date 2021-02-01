6189909ac57bace2db5f832f2789e16e8de8afbe
John Maxwell <jgwmaxwell@gmail.com>
2021-02-01 20:14:24 +0000

    temporarily switch to Argon2

--- lib/devise/encryptor.rb
+++ lib/devise/encryptor.rb
@@ -1,24 +1,23 @@
 # frozen_string_literal: true
 
-require 'bcrypt'
+require 'argon2'
 
 module Devise
   module Encryptor
     def self.digest(klass, password)
-      if klass.pepper.present?
-        password = "#{password}#{klass.pepper}"
-      end
-      ::BCrypt::Password.create(password, cost: klass.stretches).to_s
+      password = "#{password}#{klass.pepper}" if klass.pepper.present?
+      ::Argon2::Password.create(password)
     end
 
     def self.compare(klass, hashed_password, password)
       return false if hashed_password.blank?
-      bcrypt   = ::BCrypt::Password.new(hashed_password)
-      if klass.pepper.present?
-        password = "#{password}#{klass.pepper}"
+
+      if hashed_password.start_with?('$argon2')
+        password = "#{password}#{klass.pepper}" if klass.pepper.present?
+        ::Argon2::Password.verify_password(password, hashed_password)
+      else
+        super
       end
-      password = ::BCrypt::Engine.hash_secret(password, bcrypt.salt)
-      Devise.secure_compare(password, hashed_password)
     end
   end
 end
